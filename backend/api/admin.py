96f32cd0a22e45040429187c28e40cc674df756d
unknown <elijahkurien05@gmail.com>
2021-09-18 12:23:43 -0400

    products functionality added

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'Slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['Title', 'Author', 'Slug', 'Price',]
+    list_editable = ['Price']
+    prepopulated_fields = {'slug': ('title',)}
