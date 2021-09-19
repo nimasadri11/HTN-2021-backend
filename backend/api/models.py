e1acc671905417aef0f02f31076cbfc0a306ee48
Nima Sadri <nimasadri11@gmail.com>
2021-09-18 21:41:44 -0400

    added modes

---
+++ backend/api/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index = True)
+    slug = models.SlugField(max_length=255, unique = True)
+
+    class Meta: 
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name = 'product', on_delete = models.CASCADE)
+    title = models.CharField(max_length = 255)
+    description = models.TextField(blank = True)
+    image = models.ImageField(upload_to = 'images/')
+    slug = models.SlugField(max_length = 255)
+    price = models.DecimalField(max_digits = 4, decimal_places = 2)
+    count = models.IntegerField(default=0)
+    cart_count = models.IntegerField(default=0)
+
+
+    class meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
